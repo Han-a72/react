70a6d0b7b42ff4f463e618fe8498a6538879fd6a
Hana <hanaaman921@gmial.com>
2024-11-14 18:05:52 +0300

    first commit on js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import Assignmnet from './Assignmnet'
+import Home from './Home'
+import Features from './Features'
+import Enterprise from './Enterprise'
+import Support from './Support'
 function App(){
   return(
 <>
 
 <Assignmnet/>
+<Enterprise/>
+<Features/>
+<Home/>
+<Support/>
 </>
 
 
