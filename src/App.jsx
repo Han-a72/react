0541ccc1112c4ea09f7098566f5409e9e3370983
Hana <hanaaman921@gmial.com>
2024-11-18 11:31:27 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,58 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Navbar';
-import Assignment from './Assignment';
-import Home from './Home';
-import Features from './Features';
-import Enterprise from './Enterprise';
-import Support from './Support';
+
 
 function App() {
   return (
-    <Router>
- 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/assignment" element={<Assignment />} />
-        <Route path="/enterprise" element={<Enterprise />} />
-        <Route path="/features" element={<Features />} />
-        <Route path="/support" element={<Support />} />
-      </Routes>
-    </Router>
+    <>
+      <div className="nav">
+        <h2>Company name</h2>
+        <nav>
+          <a  href="">Features</a>
+          <a href="">Enterprise</a>
+          <a  href="">Support</a>
+          <a  href="">Home</a>
+        </nav>
+      </div>
+      <hr className="line-1" />
+      <div className="body">
+        <h1>Features</h1>
+      </div>
+      <br />
+      <br />
+      <br />
+      <hr className="line-2" />
+      <div className="footer">
+        <div className="part-1">
+          <h2>Features</h2>
+          <a href="#">
+            <ol>Cool Stuff</ol>
+          </a>
+          <a href="#">
+            <ol>Random Feature</ol>
+          </a>
+          <a href="#">
+            <ol>Team Feature</ol>
+          </a>
+        </div>
+        <div className="part-2">
+          <h2>Resources</h2>
+          <a href="#">
+            <ol>Resource</ol>
+          </a>
+          <a href="#">
+            <ol>Resource Name</ol>
+          </a>
+        </div>
+        <div className="part-3">
+          <h2>About</h2>
+          <a href="#">
+            <ol>Team</ol>
+          </a>
+          <a href="#">
+            <ol>Location</ol>
+          </a>
+        </div>
+      </div>
+    </>
   );
 }
 
